8f7a88801535b2cdd75cbfd82df925457d8f0a1b
arif05rachman <arif05rachman@gmail.com>
2019-07-30 00:04:22 +0700

    edit switch case

--- JAVASCRIPT/switchcase.js
+++ JAVASCRIPT/switchcase.js
@@ -1,5 +1,5 @@
 var tanggal=10; // assign nilai variabel tanggal disini! (dengan angka antara 1 - 31)
-var bulan=22; // assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
+var bulan=2; // assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
 var tahun=12; // assign nilai variabel tahun disini! (dengan angka antara 1900 - 2200)
 switch(bulan) {
     case 1:
