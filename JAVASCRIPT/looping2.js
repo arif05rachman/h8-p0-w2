dfa8295b8d8754a2e301004d79f733d9f58d9b06
arif05rachman <arif05rachman@gmail.com>
2019-08-01 18:15:58 +0700

    koreksi looping2.js

--- JAVASCRIPT/looping2.js
+++ JAVASCRIPT/looping2.js
@@ -15,11 +15,20 @@ for (i = 1; i <= rows2; i++){
     bintang='';
 }
 //3. Menyusun Barisan Tangga Bintang Dengan Nested Looping
-var rows3=5; // input the number of rows
+/*var rows3=5; // input the number of rows
 var bintang2='*'
 for (k = 1; k <= rows3; k++){
     for (k = 1; k <= rows3; k++){
         console.log(bintang2);
         bintang2+='*'
     }
+}*/
+var rows3 = 5;
+
+for(var k = 1; k <= rows3; k++) {
+    var bintang2 = '';
+    for(var l = 1; l <= k; l++) {
+        bintang2 += '*';        
+    }
+    console.log(bintang2);
 }
