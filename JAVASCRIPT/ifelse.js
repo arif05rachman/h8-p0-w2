735373f32170ab8a3a61046c6d6a247c091423ec
arif05rachman <arif05rachman@gmail.com>
2019-08-01 17:31:45 +0700

    koreksi ifesle.js

--- JAVASCRIPT/ifelse.js
+++ JAVASCRIPT/ifelse.js
@@ -1,22 +1,35 @@
-var nama = "";
-var peran = "";
-
-if( nama === "" && peran === "") {
-    console.log("Nama harus diisi!");
+var nama = 'Arif';
+var peran = 'Tank';
+if(nama===''){
+  console.log('Nama harus diisi!');
+}else if(nama!=='' && peran===''){
+  console.log('Hallo '+nama+', pilih peranmu untuk memulai game!')
+}else if(nama!=='' && peran==='Ksatria'){
+  console.log('Selamat datang di dunia Proxytia, '+nama+'. Halo '+peran+' '+nama+', kamu dapat menyerang dengan senjatamu!');
+}else if(nama !== '' && peran === 'Tabib') {
+  console.log('Selamat datang di dunia Proxytia, '+nama+'. Halo '+peran+' '+nama+', kamu akan membantu temanmu yang terluka.');
+}else if( nama !== '' && peran === 'Penyihir') {
+  console.log('Selamat datang di dunia Proxytia, '+nama+'. Halo '+peran+' '+nama+', ciptakan keajaiban yang membantu kemenanganmu!');
+}else{
+  console.log('Peran harus di isi Ksatria/Tabib/Penyihir')
+}
+/*
+if( nama === '' && peran === '') {
+    console.log('Nama harus diisi!');
 }
 
-if( nama === "Mikael") {
-  console.log("Halo " +nama+", pilih peranmu untuk memulai game!");
+if( nama === 'Mikael') {
+  console.log('Halo ' +nama+', pilih peranmu untuk memulai game!');
 }
 
-if( nama === "Nina" && peran === "Ksatria") {
-    console.log("Selamat datang di dunia Proxytia "+nama+". Halo "+peran+" "+nama+", kamu dapat menyerang dengan senjatamu!");
+if( nama === 'Nina' && peran === 'Ksatria') {
+    console.log('Selamat datang di dunia Proxytia '+nama+'. Halo '+peran+' '+nama+', kamu dapat menyerang dengan senjatamu!');
 }
 
-if(nama === "Danu" && peran === "Tabib") {
-    console.log("Selamat datang di dunia Proxytia, "+nama+". Halo "+peran+" "+nama+", kamu akan membantu temanmu yang terluka.");
+if(nama === 'Danu' && peran === 'Tabib') {
+    console.log('Selamat datang di dunia Proxytia, '+nama+'. Halo '+peran+' '+nama+', kamu akan membantu temanmu yang terluka.');
 }
 
-if( nama === "Zero" && peran === "Penyihir") {
-    console.log("Selamat datang di dunia Proxytia, "+nama+". Halo "+peran+" "+nama+", ciptakan keajaiban yang membantu kemenanganmu!");
-}
+if( nama === 'Zero' && peran === 'Penyihir') {
+    console.log('Selamat datang di dunia Proxytia, '+nama+'. Halo '+peran+' '+nama+', ciptakan keajaiban yang membantu kemenanganmu!');
+}*/
